d67e82b0fbebd0691c92eb0e42ed7f791b8b521a
vladimiriii <brett.romero@hotmail.com>
2015-11-24 15:46:44 +0100

    Updates to MosaicData problem data output

--- bin/mosaicData.py
+++ bin/mosaicData.py
@@ -7,11 +7,12 @@ import json
 class MosaicData:
     
     def __init__(self, data_filepath, data_type, sat_or_dis = None, mapping_filepath = None, values_filepath = None):
-            # data_type can be 'raw', 'consolidated' or 'problems' 
+            # data_type can be 'raw', 'consolidated', 'problems' and 'whitelist'
             # sat_or_dis can be 's' or 'd'
 
             self.data_path = data_filepath
             self.data_type = data_type
+            self.status = 'created'
             
             # Check sat_or_dis is 'd' or 's'
             if data_type == 'consolidated':
@@ -31,40 +32,52 @@ class MosaicData:
                     self.values = values_filepath
 
     def import_raw_data(self):
-        if self.data_type == 'raw':
-            # Import raw data
-            self.data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=0, dtype=str)
+        if self.status == 'created':
+            if self.data_type == 'raw':
+                # Import raw data
+                self.data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=0, dtype=str)
 
-            # Import lookup
-            mapping = pd.read_csv(self.mapping, header=0, delimiter=',', quoting=1, index_col=0)
+                # Import lookup
+                mapping = pd.read_csv(self.mapping, header=0, delimiter=',', quoting=1, index_col=0)
 
-            # Extract appropriate columns
-            questions = list(mapping['question'])
-            data = data[questions]
+                # Extract appropriate columns
+                questions = list(mapping['question'])
+                data = data[questions]
 
-            # Update Column Names
-            column_names = list(mapping['refined_name'])
-            data.columns = column_names
-            self.data = data
+                # Update Column Names
+                column_names = list(mapping['refined_name'])
+                data.columns = column_names
+                self.data = data
+                self.status = 'imported'
+            else:
+                print('MosaicData object has wrong datatype for this method.')
         else:
-            print('MosaicData object has wrong datatype for this method.')
+            print('MosaicData object has to be in \'created\' status to use this method. Currently in \'' + self.status + '\' status.')
     
     def import_consolidated_data(self):
-        if self.data_type == 'consolidated':
-            # Import consolidated data
-            data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=None)
+        if self.status == 'created':
+            if self.data_type == 'consolidated':
+                # Import consolidated data
+                data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=None)
             
-            self.data = data
-        else: 
-            print('MosaicData object has wrong datatype for this method.')
+                self.data = data
+                self.status = 'imported'
+            else: 
+                print('MosaicData object has wrong datatype for this method.')
+        else:
+            print('MosaicData object has to be in \'created\' status to use this method. Currently in \'' + self.status + '\' status.')
 
     def import_problems_data(self):
-        if self.data_type == 'problems':
-            # Import consolidated data
-            data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=None)
-            self.data = data
-        else: 
-            print('MosaicData object has wrong datatype for this method.')
+        if self.status == 'created':
+            if self.data_type == 'problems':
+                # Import consolidated data
+                data = pd.read_csv(self.data_path, header=0, delimiter=',', quoting=1, index_col=None)
+                self.data = data
+                self.status = 'imported'
+            else: 
+                print('MosaicData object has wrong datatype for this method.')
+        else:
+            print('MosaicData object has to be in \'created\' status to use this method. Currently in \'' + self.status + '\' status.')
 
     def convert_to_values(self, value_method):
         data = self.data
@@ -102,125 +115,136 @@ class MosaicData:
             try:
                 data[col] = data[col].astype(float)
             except:
-                print('Warning: At least 1 value in column '' + col + '' could not be converted to a numeric datatype.')
+                print('Warning: At least 1 value in column ' + col + ' could not be converted to a numeric datatype.')
         self.data = data
 
     def aggregate_scores(self, aggregate_by):
         return self.data.groupby(aggregate_by).mean()
         
     def transform_consolidated_data(self, year_col = 'year', indicator_col = 'indicator', scalar = 1):
-        if self.data_type == 'consolidated':
-            data = self.data
+        if self.status == 'imported':
+            if self.data_type == 'consolidated':
+                data = self.data
         
-            # Get unique year values
-            years = data[year_col].drop_duplicates().values.tolist()
-            years = sorted(years)
-            final_data = []
+                # Get unique year values
+                years = data[year_col].drop_duplicates().values.tolist()
+                years = sorted(years)
+                final_data = []
         
-            # Get Column List
-            all_cols = list(data.columns)
-            all_cols.remove(year_col)
-            all_cols.remove(indicator_col)
+                # Get Column List
+                all_cols = list(data.columns)
+                all_cols.remove(year_col)
+                all_cols.remove(indicator_col)
         
-            # Loop through years, pull out data, transpose and store in list
-            for year in years:
-                year_data = data.loc[data[year_col] == year, ]
-                reindexed_data = year_data.set_index(keys = indicator_col, drop=True)[all_cols]
+                # Loop through years, pull out data, transpose and store in list
+                for year in years:
+                    year_data = data.loc[data[year_col] == year, ]
+                    reindexed_data = year_data.set_index(keys = indicator_col, drop=True)[all_cols]
             
-                # Transpose the data and multiply by optional scalar
-                transposed_data = reindexed_data.transpose() * scalar
-                result_set = {'year' : year,
-                'data' : transposed_data}
-                final_data.append(result_set)
+                    # Transpose the data and multiply by optional scalar
+                    transposed_data = reindexed_data.transpose() * scalar
+                    result_set = {'year' : year,
+                    'data' : transposed_data}
+                    final_data.append(result_set)
             
-            self.data = final_data
-        else: 
-            print('MosaicData object requires data_type = \'consolidated\' to use this method.')
+                self.data = final_data
+                self.status = 'transformed'
+            else: 
+                print('MosaicData object requires data_type = \'consolidated\' to use this method.')
+        else:
+            print('MosaicData object has to be in \'imported\' status to use this method. Currently in \'' + self.status + '\' status.')
     
     def transform_problems_data(self, year_col = 'year', problem_col = 'problem', scalar = 1):
-        if self.data_type == 'problems':
-            data = self.data
+        if self.status == 'imported':
+            if self.data_type == 'problems':
+                data = self.data
             
-            # Get unique year values
-            years = data[year_col].drop_duplicates().values.tolist()
-            years = sorted(years)
-            final_data = []
+                # Get unique year values
+                years = data[year_col].drop_duplicates().values.tolist()
+                years = sorted(years)
+                final_data = []
         
-            # Get muncipalities List
-            municipalities = list(data.columns)
-            municipalities.remove(year_col)
-            municipalities.remove(problem_col)
+                # Get muncipalities List
+                municipalities = list(data.columns)
+                municipalities.remove(year_col)
+                municipalities.remove(problem_col)
             
             
-            # Scale Data
-            data[municipalities] = data[municipalities]*scalar
-            
-            # Loop through municipalities 
-            for muni in municipalities:
-                muni_data = data.loc[ : , [year_col, problem_col, muni]]
-                muni_data = muni_data.loc[muni_data[muni].notnull(),]
+                # Scale Data
+                data[municipalities] = data[municipalities]*scalar
                 
-                
-                # Loop through years
-                for year in years:
-                    year_data = muni_data.loc[data[year_col] == year, [problem_col, muni]]
-                    year_data.columns = ['label', 'value']
-                    year_data = year_data.to_dict(orient = 'records')
+                # Loop through municipalities 
+                for muni in municipalities:
+                    result_set = {muni : {}}
+                    muni_data = data.loc[ : , [year_col, problem_col, muni]]
+                    muni_data = muni_data.loc[muni_data[muni].notnull(),]
                     
-                    # Create dictionary with values
-                    result_set = {'year' : year, 
-                    'municipality' : muni,
-                    'values' : year_data
-                    }
-                final_data.append(result_set)
+                    
+                    # Loop through years
+                    for year in years:
+                        year_data = muni_data.loc[data[year_col] == year, [problem_col, muni]]
+                        year_data.columns = ['label', 'value']
+                        year_data = year_data.to_dict(orient = 'records')
+                    
+                        # Create dictionary with values
+                        result_set[muni][year] = year_data
+                    
+                    final_data.append(result_set)
             
-            self.data = final_data
-        else: 
-            print('MosaicData object requires data_type = \'problems\' to use this method.')
+                self.data = final_data
+                self.status = 'transformed'
+                
+            else: 
+                print('MosaicData object requires data_type = \'problems\' to use this method.')
+        else:
+            print('MosaicData object has to be in \'imported\' status to use this method. Currently in \'' + self.status + '\' status.')
     
     def output_data(self, output_file, output_type = 'json'):
-        data = self.data
-        final_dict = {}
+        if self.status == 'transformed':
+            data = self.data
+            final_dict = {}
         
-        # Consolidated data type
-        if self.data_type == 'consolidated':
-            for entries in data:
-                year = entries['year']
+            # Consolidated data type
+            if self.data_type == 'consolidated':
+                for entries in data:
+                    year = entries['year']
                 
-                # Is the data satisfied or dissatisfied percentage?
-                if self.sat_or_dis == 's':
-                    file_path = output_file + str(year) + 'S'
-                elif self.sat_or_dis == 'd':
-                    file_path = output_file + str(year) + 'D'
+                    # Is the data satisfied or dissatisfied percentage?
+                    if self.sat_or_dis == 's':
+                        file_path = output_file + str(year) + 'S'
+                    elif self.sat_or_dis == 'd':
+                        file_path = output_file + str(year) + 'D'
                     
-                # If CSV, return individual files
-                if output_type == 'csv':
-                    file_path = file_path + '.csv'
-                    entries['data'].to_csv(path_or_buf = file_path, force_ascii = False) 
-                else: 
-                    final_dict[year] = entries['data'].to_dict(orient='index')
-            
-            # If JSON, return merged JSON file
-            if output_type == 'json':
-                if self.sat_or_dis == 's':
-                    file_path = '../data/clean_data/satisfied.json'
-                elif self.sat_or_dis == 'd':
-                    file_path = '../data/clean_data/dissatisfied.json'
+                    # If CSV, return individual files
+                    if output_type == 'csv':
+                        file_path = file_path + '.csv'
+                        entries['data'].to_csv(path_or_buf = file_path, force_ascii = False) 
+                    else: 
+                        final_dict[year] = entries['data'].to_dict(orient='index')
+                
+                # If JSON, return merged JSON file
+                if output_type == 'json':
+                    if self.sat_or_dis == 's':
+                        file_path = '../data/clean_data/satisfied.json'
+                    elif self.sat_or_dis == 'd':
+                        file_path = '../data/clean_data/dissatisfied.json'
+                    with open(file_path, 'w') as data_file:
+                        json.dump(obj = final_dict, fp = data_file, indent = 4, sort_keys = True)
+        
+            # Problem data type
+            if self.data_type == 'problems': 
+                file_path = output_file + 'problems.json'
                 with open(file_path, 'w') as data_file:
-                    json.dump(obj = final_dict, fp = data_file, indent = 4, sort_keys = True)
+                    json.dump(obj = data, fp = data_file, indent = 4, sort_keys = True)
         
-        # Problem data type
-        if self.data_type == 'problems': 
-            file_path = output_file + 'problems.json'
-            with open(file_path, 'w') as data_file:
-                json.dump(obj = data, fp = data_file, indent = 4, sort_keys = True)
-        
-        # Raw Datatype
-        elif self.data_type == 'raw':
-            if output_type == 'csv':
-                data.to_csv(path_or_buf = output_file, force_ascii = False)
-            else:
-                data.to_json(orient = 'index', path_or_buf = output_file, force_ascii = False)
+            # Raw Datatype
+            elif self.data_type == 'raw':
+                if output_type == 'csv':
+                    data.to_csv(path_or_buf = output_file, force_ascii = False)
+                else:
+                    data.to_json(orient = 'index', path_or_buf = output_file, force_ascii = False)
+        else:
+            print('MosaicData object has to be in \'transformed\' status to use this method. Currently in \'' + self.status + '\' status.')
     
     def delete_old_files(self, folder = '../data/clean_data/'):
         for the_file in os.listdir(folder):
